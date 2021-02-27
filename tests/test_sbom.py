6bb0e7b536bbc6c61b385d44df6871e148701be3
Stefan Hagen <stefan@hagen.link>
2021-02-27 14:53:58 +0100

    Added TODO documentation test for upstream XML validation harness

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -3,11 +3,14 @@
 import pathlib
 import pytest  # type: ignore
 
+import xmlschema
 import cyclonedx.bom.validator as cdx
 
 CDX_MVP_JSON_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom-minimal-schema-match.json")
 CDX_TYPICAL_JSON_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom.json")
 EMPTY_JSON_OBJECT_PATH = pathlib.Path("examples", "empty_object.json")
+
+CDX_TYPICAL_XML_1_0_PATH = pathlib.Path("examples", "cyclonedx-v1.0_sbom.xml")
 
 
 def test_deps_nok_cyclone_dx_validation_of_json_empty_object(capsys):
@@ -26,3 +29,19 @@ def test_deps_ok_cyclone_dx_validation_of_typical_json_1_2(capsys):
     assert cdx.is_valid(CDX_TYPICAL_JSON_1_2_PATH, True) is True
     out, _ = capsys.readouterr()
     assert not out.strip()
+
+
+def test_deps_nok_cyclone_dx_validation_of_typical_xml_1_0():
+    """
+    TODO(sthagen) - consider issuing a PR at cyclonedx-bom upstream project
+      Proposal is to change from v0.4.3 behavior to better use str(path) when
+      calling further upstream xmlschema.XMLSchema(schema_path).is_valid(path)
+    """
+    message = (
+        r"wrong type <class 'pathlib.PosixPath'> for 'source' attribute:"
+        r" an ElementTree object or an Element instance or a string containing"
+        r" XML data or an URL or a file-like object is required."
+    )
+    with pytest.raises(xmlschema.exceptions.XMLSchemaTypeError, match=message):
+        cdx.is_valid(CDX_TYPICAL_XML_1_0_PATH, False)
+
