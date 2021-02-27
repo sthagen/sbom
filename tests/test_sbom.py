6698dbb6bbc601f58840d365f99a5b481518e4da
Stefan Hagen <stefan@hagen.link>
2021-02-27 14:36:29 +0100

    Added positive test for cyclone dx dependency accepting MVP JSON v1.2 object

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -5,6 +5,7 @@ import pytest  # type: ignore
 
 import cyclonedx.bom.validator as cdx
 
+CDX_MVP_JSON_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom-minimal-schema-match.json")
 EMPTY_JSON_OBJECT_PATH = pathlib.Path("examples", "empty_object.json")
 
 
@@ -12,3 +13,9 @@ def test_deps_nok_cyclone_dx_validation_of_json_empty_object(capsys):
     assert cdx.is_valid(EMPTY_JSON_OBJECT_PATH, True) is False
     out, _ = capsys.readouterr()
     assert "'bomFormat' is a required property" in out
+
+
+def test_deps_ok_cyclone_dx_validation_of_mvp_json_1_2(capsys):
+    assert cdx.is_valid(CDX_MVP_JSON_1_2_PATH, True) is True
+    out, _ = capsys.readouterr()
+    assert not out.strip()
