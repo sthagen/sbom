671b67362c3ca9cac63c285d221a218018aab96f
Stefan Hagen <stefan@hagen.link>
2021-02-27 14:31:34 +0100

    Added first test - negative for cyclone dx dependency rejecting empty JSON object

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+import pathlib
 import pytest  # type: ignore
+
+import cyclonedx.bom.validator as cdx
+
+EMPTY_JSON_OBJECT_PATH = pathlib.Path("examples", "empty_object.json")
+
+
+def test_deps_nok_cyclone_dx_validation_of_json_empty_object(capsys):
+    assert cdx.is_valid(EMPTY_JSON_OBJECT_PATH, True) is False
+    out, _ = capsys.readouterr()
+    assert "'bomFormat' is a required property" in out
