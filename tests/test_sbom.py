99fc395c8a9ead3bf73209da0912758b70c682d1
Stefan Hagen <stefan@hagen.link>
2021-02-27 19:09:54 +0100

    Added monkey patched SPDX v2.2 Tag/Value format test - spdx-tools v0.6.1 do neither seem to be useful nor fit for v2.2

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -6,13 +6,33 @@ import pytest  # type: ignore
 import xmlschema
 import cyclonedx.bom.validator as cdx
 
-CDX_MVP_JSON_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom-minimal-schema-match.json")
-CDX_TYPICAL_JSON_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom.json")
-EMPTY_JSON_OBJECT_PATH = pathlib.Path("examples", "empty_object.json")
+import spdx
+from spdx.parsers.tagvalue import Parser
+from spdx.parsers.loggers import StandardLogger
+from spdx.parsers.tagvaluebuilders import Builder
 
-CDX_TYPICAL_XML_1_0_PATH = pathlib.Path("examples", "cyclonedx-v1.0_sbom.xml")
+
+def _spdx_tv_validation_proxy(tv_file_path):
+    """YAGNI"""
+    p = Parser(Builder(), StandardLogger())
+    p.build()
+    with open(tv_file_path) as f:
+        data = f.read()
+        document, error = p.parse(data)
+        return False if error else True
+
+
+EXAMPLES_PATH = pathlib.Path("examples")
+CDX_MVP_JSON_1_2_PATH = pathlib.Path(EXAMPLES_PATH, "cyclonedx-v1.2_sbom-minimal-schema-match.json")
+CDX_TYPICAL_JSON_1_2_PATH = pathlib.Path(EXAMPLES_PATH, "cyclonedx-v1.2_sbom.json")
+EMPTY_JSON_OBJECT_PATH = pathlib.Path(EXAMPLES_PATH, "empty_object.json")
+
+CDX_TYPICAL_XML_1_0_PATH = pathlib.Path(EXAMPLES_PATH, "cyclonedx-v1.0_sbom.xml")
 # TODO(sthagen) add test for v1.2 validation - requires the Apache 2.0 licensed v1.2 XSDs ...
-CDX_TYPICAL_XML_1_2_PATH = pathlib.Path("examples", "cyclonedx-v1.2_sbom.xml")
+CDX_TYPICAL_XML_1_2_PATH = pathlib.Path(EXAMPLES_PATH, "cyclonedx-v1.2_sbom.xml")
+
+# TODO(sthagen) implement independent SPDX v2.2 validation
+SPDX_TYPICAL_TV_2_2_PATH = pathlib.Path(EXAMPLES_PATH, "spdx-v2.2_sbom_tag-value.txt")
 
 
 def test_deps_nok_cyclone_dx_validation_of_json_empty_object(capsys):
@@ -55,3 +75,17 @@ def test_deps_ok_cyclone_dx_validation_of_typical_xml_1_0_patched(capsys):
     assert cdx.is_valid(str(CDX_TYPICAL_XML_1_0_PATH), False) is True
     out, _ = capsys.readouterr()
     assert not out.strip()
+
+
+def test_deps_ok_spdx_validation_of_typical_tv_2_2(capsys):
+    assert _spdx_tv_validation_proxy(SPDX_TYPICAL_TV_2_2_PATH) is False  # Cf. SPDX TODO above
+    gibberish = (
+        "true\n"
+        "Package copyright text must be free form text, line: 21\n"
+        "Found unknown tag : Relationship at line: 23\n"
+        "FileChecksum must be a single line of text starting with 'SHA1:', line:29\n"
+        "FileChecksum must be a single line of text starting with 'SHA1:', line:30\n"
+        "FileCopyrightText must be one of NOASSERTION, NONE or free form text, line: 33"
+    )
+    out, _ = capsys.readouterr()
+    assert out.strip() == gibberish
