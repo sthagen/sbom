3b00a4efa7e55f00e02b6d4109040dae557a55e6
Stefan Hagen <stefan@hagen.link>
2021-03-13 08:07:20 +0100

    Added future requirements to setup tools interface

--- setup.py
+++ setup.py
@@ -31,7 +31,10 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
+        "attrs",
+        "click",
         "cyclonedx-bom",
+        "jsonschema",
         "spdx-tools",
         "xmlschema"
     ],
