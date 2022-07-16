65147ad1925bf8ad3041a0a3963dd5dd7c364e01
Stefan Hagen <stefan@hagen.link>
2022-07-16 21:51:07 +0200

    Implemented some further linter happiness and enhanced the module documentation

--- sbom/__main__.py
+++ sbom/__main__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Visit folder tree with CycloneDX, SPDX, or SWID documents, validate these documents against baseline as well as extensions, and generate reports."""
+"""Generic linter for software Bill of Materials (SBOM) formats.
+
+1. visit folder tree with CycloneDX, SPDX, or SWID documents
+2. validate these documents against baseline as well as extensions, and
+3. generate reports.
+"""
 import sys
 
 import sbom.sbom as lint
