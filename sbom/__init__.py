ceaefdc55a9b4e03c21836cdf4805804db5ccf2a
Stefan Hagen <stefan@hagen.link>
2023-01-07 18:38:38 +0100

    Formatted for the happiness of some

--- sbom/__init__.py
+++ sbom/__init__.py
@@ -4,4 +4,3 @@ __version__ = '2022.12.13+parent.ff7049ef'
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
-
