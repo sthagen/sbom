2cb91692e8682127c15e23d73967cb7afaf83904
Stefan Hagen <stefan@hagen.link>
2023-10-07 13:09:49 +0200

    Bumped impl version for release 2023.10.7

--- sbom/__init__.py
+++ sbom/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.7+parent.8f638cb1'
+# [[[end]]] (checksum: d31a7e4fc048d9d235192794e33ee6a0)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
